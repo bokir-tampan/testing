ba846e95b43f8bad438fa69619a84506bc94c45e
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-03-21 10:54:02 +0800

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -52,7 +52,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-curl -sS https://istriku.me/testing/resources/ascii-home
+curl -sS https://istriku.me/gratis/resources/ascii-home
 echo "XRAY Core Vmess / Vless"
 echo "Trojan / Trojan Go"
 echo "Progress..."
